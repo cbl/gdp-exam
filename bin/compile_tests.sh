19665573d2b26b447dd19f4259bbae3b6f35e561
cbl <lennart.carbe@gmail.com>
2021-02-03 20:48:20 +0100

    test shell scripts

---
+++ bin/compile_tests.sh
@@ -0,0 +1,4 @@
+g++ -Wall -std=c++17 -I . -o bin/test_database test/test_database.cpp && \
+g++ -Wall -std=c++17 -I . -o bin/test_price_calculator test/test_price_calculator.cpp && \
+g++ -Wall -std=c++17 -I . -o bin/test_stop_collection test/test_stop_collection.cpp && \
+g++ -Wall -std=c++17 -I . -o bin/test test/test.cpp
---
+++ bin/run_tests.sh
@@ -0,0 +1,4 @@
+bin/test_database && \
+bin/test_price_calculator && \
+bin/test_stop_collection && \
+bin/test
